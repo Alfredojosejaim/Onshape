94ae3ddef0563df2f57ad0c483d32c5fed1ff986
Alfredojosejaim <alfredojosejaim03@gmail.com>
2026-08-25 23:57:00 -0300

    Create __init__.py

---
+++ Topologia_Optimizada/connectors/__init__.py
@@ -0,0 +1,5 @@
+"""CAD Connectors - Optional external CAD platform integrations.
+
+This module contains optional connectors for external CAD platforms.
+These connectors are NOT required for the standalone application to function.
+"""

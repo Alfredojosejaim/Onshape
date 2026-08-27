419d8526575f6450f65b8d0ba742bad1020e1485
Alfredojosejaim <alfredojosejaim03@gmail.com>
2026-08-27 08:19:19 -0300

    Create test_sma_import.py

---
+++ Topologia_Optimizada/experimentos/kratos_topopt_poc/test_sma_import.py
@@ -0,0 +1,5 @@
+print("Before import")
+import KratosMultiphysics as Kratos
+from KratosMultiphysics import StructuralMechanicsApplication as SMA
+print("After import")
+print("SMA imported successfully")

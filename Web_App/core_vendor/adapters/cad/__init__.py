d22c1d00afed2b1ad0612ab149abee147165a672
Alfredojosejaim <alfredojosejaim03@gmail.com>
2026-09-11 08:42:14 -0300

    a

--- Web_App/core_vendor/adapters/cad/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CAD Adapters package."""
-
-from adapters.cad.base import BaseCADAdapter
-from adapters.cad.step_adapter import StepAdapter
-
-__all__ = ["BaseCADAdapter", "StepAdapter"]
--- Web_App/run_experiment.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Lanza el experimento HTML sin tocar el core.
-cd /d %~dp0
-python app_hybrid.py %*
